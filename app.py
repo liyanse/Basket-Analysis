88d7ed1cc9c274b371950caa26934debbd033443
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-02-08 22:02:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,143 +1,48 @@
-import os
-import uuid
-#from flask import*
-#import Flask
-import urllib
-import numpy as np
-from PIL import Image
-from tensorflow.keras.models import load_model
-from flask import Flask , render_template  , request , send_file
-from tensorflow.keras.preprocessing.image import load_img , img_to_array
+# -*- coding: utf-8 -*-
+"""
+"""
+##!pip install fastapi uvicorn
 
-app = Flask(__name__)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model = load_model(os.path.join(BASE_DIR , 'project_model.h5'))
+# 1. Library imports
+from urllib import response
+# from sci.demand_forecasting.app.mains import *
+import uvicorn ##ASGI
+import pickle
+import pandas as pd
+from fastapi import FastAPI
+from sklearn.preprocessing import LabelEncoder
 
+from pydantic import BaseModel
 
-ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+# 2. Create the app object
+app = FastAPI()
 
-classes = ['COVID19' ,'NORMAL', 'PNEUMONIA', 'OTHERS' ]
-
-
-def predict(filename , model):
-    img = load_img(filename , target_size = (224 , 224))
-    #img = img/255.0
-    img = img_to_array(img)
-    img = img.reshape(1 , 224 ,224 ,3)
-
-    # create a test image list
-    img = np.ones((1 , 224 ,224 ,3), dtype=np.uint8)
-    img_list = [img] * 4
-
-    # use a list comp to run through the images and change dtype
-    changed_img_list = [img.astype(np.float32) for i in img_list]
-    #img = [img.astype(np.float32) for i in img_list]
-
-    #img = img.astype('float')
-    img = img/255.0
-    result = model.predict(img)
-
-    dict_result = {}
-    for i in range(4):
-        dict_result[result[0][i]] = classes[i]
-
-    res = result[0]
-    res.sort()
-    res = res[::-1]
-    prob = res[:4]
-    
-    prob_result = []
-    class_result = []
-    for i in range(4):
-        prob_result.append((prob[i]*100).round(2))
-        class_result.append(dict_result[prob[i]])
-
-    return class_result , prob_result
+@app.get('/')
+def index():
+    return {'Welcome To Demand Forecasting API'}
 
 
+class filter(BaseModel):
+    start_date:str
+    end_date:str
 
+class product(BaseModel):
+    product_id:str
 
-@app.route('/')
-def home():
-        return render_template("index.html")
+@app.post('/predictdemand')
+async def demandpredictor(filter:filter):
+    response = filter.dict()
+    response_df = pd.DataFrame(response)
+    cols = ['inventory_id', 'product_name', 'category', 'uom', 'warehouse_id','order_date',
+        'customer_id', 'customer_name', 'county', 'subcounty']
+    response_df[cols] = response_df[cols].apply(LabelEncoder().fit_transform)
+    model = pickle.load(open("../Models/sarimaxmodel.pkl", "rb"))
+    predictions = model.predict(response_df)
+    # res = totalprods.to_json(orient="records")
+    return predictions
 
-@app.route('/success' , methods = ['GET' , 'POST'])
-def success():
-    error = ''
-    target_img = os.path.join(os.getcwd() , 'static/images')
-    if request.method == 'POST':
-        if(request.form):
-            link = request.form.get('link')
-            try :
-                resource = urllib.request.urlopen(link)
-                unique_filename = str(uuid.uuid4())
-                filename = unique_filename+".jpg"
-                img_path = os.path.join(target_img , filename)
-                output = open(img_path , "wb")
-                output.write(resource.read())
-                output.close()
-                img = filename
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+#uvicorn main:app --reload
 
-                class_result , prob_result = predict(img_path , model)
-
-                predictions = {
-                      "class1":class_result[0],
-                        "class2":class_result[1],
-                        "class3":class_result[2],
-                        "class4":class_result[3],
-                        "prob1": prob_result[0],
-                        "prob2": prob_result[1],
-                        "prob3": prob_result[2],
-                        "prob4": prob_result[3],
-                }
-
-            except Exception as e : 
-                print(str(e))
-                error = 'This image from this site is not accesible or inappropriate input'
-
-            if(len(error) == 0):
-                return  render_template('success.html' , img  = img , predictions = predictions)
-            else:
-                return render_template('index.html' , error = error) 
-
-            
-        elif (request.files):
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                file.save(os.path.join(target_img , file.filename))
-                img_path = os.path.join(target_img , file.filename)
-                img = file.filename
-
-                class_result , prob_result = predict(img_path , model)
-
-                predictions = {
-                      "class1":class_result[0],
-                        "class2":class_result[1],
-                        "class3":class_result[2],
-                        "class4":class_result[3],
-                        "prob1": prob_result[0],
-                        "prob2": prob_result[1],
-                        "prob3": prob_result[2],
-                        "prob4": prob_result[3],
-                }
-
-            else:
-                error = "Please upload images of jpg , jpeg and png extension only"
-
-            if(len(error) == 0):
-                return  render_template('success.html' , img  = img , predictions = predictions)
-            else:
-                return render_template('index.html' , error = error)
-
-    else:
-        return render_template('index.html')
-
-
-HOST="127.0.0.1"
-PORT=8000
-if __name__ == "__main__":
-    app.run(host=HOST,port=PORT,debug=True)
 
