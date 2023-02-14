2ae8a4f36fb351bbee161a835fe8389790c0c2d5
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-02-14 13:56:23 +0300

    Valentines

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
 from fastapi import FastAPI
-from mlxtend.frequent_patterns import apriori
-from mlxtend.frequent_patterns import association_rules
 from pydantic import BaseModel
+from mlxtend.frequent_patterns import apriori
+import pickle
 
-# 2. Create the app object
+# Initialize the FastAPI
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'Welcome To Our Product Recommender API'}
+# Load the pre-trained apriori model
+model = pickle.load(open('basketAnalysis.pickle', 'rb'))
 
-class filter(BaseModel):
-    start_date:str
-    end_date:str
+# Create a class to store the user input
+class UserInput(BaseModel):
+    product: str
 
-class product(BaseModel):
-    product_id:str
-
-@app.post('/predictdemand')
-async def product_rec(dataframe, product, stop_num = 3):
-    counter = 0
-    rec_list = []
-    for index, row in enumerate(dataframe["antecedents"]):        
-        for item in list(row):
-            if item == product:
-                rec_list.append(list(dataframe["consequents"][index])[0])
-                counter += 1
-                if counter == stop_num:
-                    break
-    return rec_list
-product_rec(rules, "soda")
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
-#uvicorn main:app --reload
+# Create a route for the API
+@app.post("/predict")
+def predict(user_input: UserInput):
+    # Get the user input
+    product = user_input.product
+    
+    # Make predictions using the model
+    predictions = model.apriori(product)
+    
+    # Return the prediction
+    return {
+        "Product": product,
+        "Recommendations": predictions
+    }
