658a0eeb12d0f4dde534526169ccc87aae980bba
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-02-18 17:21:10 +0300

    deploy

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = FastAPI()
 
 with open('apriori_model.pkl','rb')as f:
     rules = pickle.load(f)
-
-@app.get('/products/{product_name}')
-def similar_products(product_name:  str):
-    results = [rule[2] for rule in rules if rule[1] == product_name]
     
-    return{'similar products are': results}
-       
-    
+ 
+@app.get("/recommendations/{product}")
+def get_recommendations(product: str):
+    # Get the recommendations based on the input product
+    recommendations = [x for x in rules if product in x[0]]
+    # Return the recommendations
+    return {"recommendations": recommendations}
