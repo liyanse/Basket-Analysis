07d123572188eb4706952ce0c0fed7fe33d400da
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-02-20 15:01:45 +0300

    deploy

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+##Importing the necessary libraries
 from fastapi import FastAPI
 import pickle
 
+## Create FASTAPI instance
 app = FastAPI()
 
+## opening the pickle file for predictions
 with open('apriori_model.pkl','rb')as f:
     rules = pickle.load(f)
     
+# Define the GET request endpoint
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Recommendations platform"}
+    
  
-@app.get("/recommendations/{product}")
-def get_recommendations(product: str):
+@app.post("/recommendations/{product}")
+async def get_recommendations(product:str):
     # Get the recommendations based on the input product
     recommendations = [x for x in rules if product in x[0]]
     # Return the recommendations
